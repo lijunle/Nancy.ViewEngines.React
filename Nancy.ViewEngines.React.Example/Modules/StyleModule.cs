0595342825365684d33d1e6486d870c7c2d958ea
Junle Li <lijunle@gmail.com>
2015-08-23 19:24:31 +0800

    Add title and style convention modules.

---
+++ Nancy.ViewEngines.React.Example/Modules/StyleModule.cs
@@ -0,0 +1,31 @@
+﻿namespace Nancy.ViewEngines.React.Example.Modules
+{
+    using System.Collections.Generic;
+    using Responses;
+
+    public class StyleModule : NancyModule
+    {
+        public StyleModule()
+        {
+            this.Get["style"] = _ => new TestModel("Background should be yellow.");
+
+            this.Get["style/css"] = _ => new TextResponse("code { background: yellow }", "text/css");
+        }
+
+        private class TestModel
+        {
+            public TestModel(string text)
+            {
+                this.Text = text;
+            }
+
+            public string Text { get; }
+
+            public IEnumerable<string> Styles =>
+                new[]
+                {
+                    "/style/css"
+                };
+        }
+    }
+}
---
+++ Nancy.ViewEngines.React.Example/Modules/TitleModule.cs
@@ -0,0 +1,22 @@
+﻿namespace Nancy.ViewEngines.React.Example.Modules
+{
+    public class TitleModule : NancyModule
+    {
+        public TitleModule()
+        {
+            this.Get["title"] = _ => new TestModel("This is title");
+        }
+
+        private class TestModel
+        {
+            public TestModel(string title)
+            {
+                this.Title = title;
+            }
+
+            public string Title { get; }
+
+            public string Text => "Render title.";
+        }
+    }
+}
