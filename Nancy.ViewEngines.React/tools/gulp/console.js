776808c6d95c656f05f9e6bfef5cb228ffb35eba
Junle Li <lijunle@gmail.com>
2015-08-24 03:01:13 +0800

    Hack on console only on server render.
    - Resolve client console.log not work issue.

--- Nancy.ViewEngines.React/tools/gulp/console.js
+++ Nancy.ViewEngines.React/tools/gulp/console.js
@@ -1,7 +1,8 @@
 // Avoid `console` errors in environments that lack a console.
 // Inspired from https://github.com/matthewhudson/console/blob/master/console.js
 
-const logger = {};
+const inServer = typeof document === 'undefined';
+const logger = inServer ? {} : window.console;
 const logs = {};
 
 [
@@ -9,18 +10,18 @@ const logs = {};
   'groupCollapsed', 'groupEnd', 'markTimeline', 'profile', 'profileEnd',
   'table', 'time', 'timeEnd', 'timeStamp'
 ].forEach(method => {
-  logger[method] = () => {};
+  logger[method] = logger[method] || () => {};
 });
 
 // restore the log arguements to redirect information for client
 [
   'log', 'dir', 'trace', 'debug', 'info', 'warn', 'error'
 ].forEach(method => {
-  logger[method] = (...args) => {
+  logger[method] = logger[method] || ((...args) => {
     const stacktrace = (new Error()).stack;
     logs[method] = logs[method] || [];
     logs[method].push([stacktrace].concat(args));
-  };
+  });
 });
 
 function formatCode(method, stacktrace, args) {
@@ -30,13 +31,15 @@ function formatCode(method, stacktrace, args) {
   return code;
 }
 
-// retrieve stored log arguements to show information back to client
-logger.restore = () => {
-  return Object.keys(logs).map(method => {
-    return logs[method].map(([stacktrace, ...args]) => {
-      return formatCode(method, stacktrace, args);
+if (inServer) {
+  // restore the logs during server render back to client
+  logger.restore = () => {
+    return Object.keys(logs).map(method => {
+      return logs[method].map(([stacktrace, ...args]) => {
+        return formatCode(method, stacktrace, args);
+      }).join('');
     }).join('');
-  }).join('');
-};
+  };
+}
 
 export default logger; // as console
