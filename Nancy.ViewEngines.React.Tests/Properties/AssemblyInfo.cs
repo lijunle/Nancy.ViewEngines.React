47756c1748fd256ffb6e48618c52c141997d6a5f
Junle Li <lijunle@gmail.com>
2015-08-26 22:58:25 +0800

    Add test project scaffolding.

---
+++ Nancy.ViewEngines.React.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nancy.ViewEngines.React.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nancy.ViewEngines.React.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1db25bf5-f80f-41a3-babf-9bc49ac94986")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
