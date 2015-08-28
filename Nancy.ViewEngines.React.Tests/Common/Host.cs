56420fe35f88879711f2b2294a228cbbd30be844
Junle Li <lijunle@gmail.com>
2015-08-29 02:11:55 +0800

    Add self hosting logic.
    - Load React and Example DLL to Nancy.
    - Set Example folder as root path.

---
+++ Nancy.ViewEngines.React.Tests/Common/Host.cs
@@ -0,0 +1,33 @@
+﻿namespace Nancy.ViewEngines.React.Tests.Common
+{
+    using System;
+    using Hosting.Self;
+
+    public class Host : IDisposable
+    {
+        internal const string Url = "http://localhost:64343";
+
+        private readonly NancyHost host;
+
+        public Host()
+        {
+            var uri = new Uri(Url);
+            var bootstrapper = new TestBootstrapper();
+            var configuration = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations
+                {
+                    CreateAutomatically = true
+                }
+            };
+
+            this.host = new NancyHost(uri, bootstrapper, configuration);
+            this.host.Start();
+        }
+
+        public void Dispose()
+        {
+            this.host.Dispose();
+        }
+    }
+}
