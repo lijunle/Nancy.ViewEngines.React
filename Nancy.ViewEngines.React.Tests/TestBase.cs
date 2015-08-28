dcb255002ad72fb3f5cf46e254656bd56c8d54c5
Junle Li <lijunle@gmail.com>
2015-08-29 02:24:55 +0800

    Add abstract test base.
    - It will start the self host before all the first.

---
+++ Nancy.ViewEngines.React.Tests/TestBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Nancy.ViewEngines.React.Tests
+{
+    using Common;
+    using Xunit;
+
+    [Collection(FixtureCollection.Name)]
+    public class TestBase
+    {
+        [CollectionDefinition(Name)]
+        public class FixtureCollection : ICollectionFixture<Host>
+        {
+            public const string Name = "host";
+        }
+    }
+}
