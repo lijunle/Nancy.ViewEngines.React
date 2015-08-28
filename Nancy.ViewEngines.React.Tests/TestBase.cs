0f9bbbc333e10013a690cf7e17b2d1ed4013e2f6
Junle Li <lijunle@gmail.com>
2015-08-29 02:45:40 +0800

    Add home page simple tests.
    - Launch Chrome from TestBase
    - Quit Chrome when TestBase dispose
    - Navigate to home page and assert on title.

--- Nancy.ViewEngines.React.Tests/TestBase.cs
+++ Nancy.ViewEngines.React.Tests/TestBase.cs
@@ -1,11 +1,26 @@
 ﻿namespace Nancy.ViewEngines.React.Tests
 {
+    using System;
     using Common;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
     using Xunit;
 
     [Collection(FixtureCollection.Name)]
-    public class TestBase
+    public class TestBase : IDisposable
     {
+        public TestBase()
+        {
+            this.Driver = new ChromeDriver();
+        }
+
+        protected IWebDriver Driver { get; }
+
+        public void Dispose()
+        {
+            this.Driver.Quit();
+        }
+
         [CollectionDefinition(Name)]
         public class FixtureCollection : ICollectionFixture<Host>
         {
