e1bc99391623e37aca2603d7d2ee3e6df40b52d6
Junle Li <lijunle@gmail.com>
2015-08-30 01:16:40 +0800

    Add style page test cases.

---
+++ Nancy.ViewEngines.React.Tests/Pages/StylePage.cs
@@ -0,0 +1,95 @@
+﻿namespace Nancy.ViewEngines.React.Tests.Pages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using OpenQA.Selenium;
+
+    internal class StylePage : PageBase
+    {
+        internal IWebElement ColorList =>
+            this.Driver.FindElement(By.TagName("ul"));
+
+        internal IEnumerable<ColorItem> ColorItems =>
+            this.ColorList.FindElements(By.TagName("li"))
+                .Select(element => new ColorItem(element));
+
+        internal IEnumerable<StyleLink> StyleLinks =>
+            this.Driver.FindElements(By.TagName("link"))
+                .Select(element => new StyleLink(element));
+
+        internal IWebElement InputBox =>
+            this.Driver.FindElement(By.CssSelector("input[type=text]"));
+
+        internal IWebElement AddButton =>
+            this.Driver.FindElement(By.CssSelector("input[type=button]"));
+
+        protected override string Path => "style";
+
+        internal class ColorItem
+        {
+            private readonly IWebElement element;
+
+            internal ColorItem(IWebElement element)
+            {
+                this.element = element;
+            }
+
+            internal string Value =>
+                this.element.GetAttribute("class");
+
+            internal string Text =>
+                this.element.Text;
+
+            internal Color BackgroundColor
+            {
+                get
+                {
+                    int[] rgba = this.element.GetCssValue("background-color")
+                        .Substring("rgba".Length)
+                        .Trim('(', ')')
+                        .Split(',')
+                        .Select(x => x.Trim())
+                        .Select(x => int.Parse(x))
+                        .ToArray();
+
+                    // ignore the alpha component value.
+                    return Color.FromArgb(rgba[0], rgba[1], rgba[2]);
+                }
+            }
+
+            internal void Remove() =>
+                this.element.FindElement(By.TagName("button")).Click();
+        }
+
+        internal class StyleLink
+        {
+            private readonly IWebElement element;
+
+            static StyleLink()
+            {
+                Comparer = new StyleLinkComparer();
+            }
+
+            internal StyleLink(IWebElement element)
+            {
+                this.element = element;
+            }
+
+            internal static IEqualityComparer<StyleLink> Comparer { get; }
+
+            internal string Href =>
+                this.element.GetAttribute("href");
+
+            private class StyleLinkComparer : IEqualityComparer<StyleLink>
+            {
+                public bool Equals(StyleLink x, StyleLink y) =>
+                    x.element.Equals(y.element);
+
+                public int GetHashCode(StyleLink styleLink) =>
+                    styleLink.element.GetHashCode();
+            }
+        }
+    }
+}
---
+++ Nancy.ViewEngines.React.Tests/StyleTests.cs
@@ -0,0 +1,91 @@
+﻿namespace Nancy.ViewEngines.React.Tests
+{
+    using System.Drawing;
+    using System.Linq;
+    using Pages;
+    using Xunit;
+
+    public class StyleTests : TestBase
+    {
+        private readonly StylePage page;
+
+        public StyleTests(Fixture fixture)
+            : base(fixture)
+        {
+            this.page = this.GoTo<StylePage>();
+        }
+
+        [Fact]
+        public void Style_page_should_return_a_list_of_colors_and_a_form()
+        {
+            Assert.NotNull(this.page.ColorList);
+            Assert.Equal(2, this.page.ColorItems.Count());
+            Assert.Equal("yellow", this.page.ColorItems.ElementAt(0).Value);
+            Assert.Equal("blue", this.page.ColorItems.ElementAt(1).Value);
+
+            Assert.NotNull(this.page.InputBox);
+            Assert.Equal("red", this.page.InputBox.GetAttribute("value"));
+
+            Assert.NotNull(this.page.AddButton);
+            Assert.Equal("Add", this.page.AddButton.GetAttribute("value"));
+        }
+
+        [Fact]
+        public void Each_color_item_should_be_with_corresponding_text_and_background_color()
+        {
+            Assert.NotEmpty(this.page.ColorItems);
+
+            foreach (var colorItem in this.page.ColorItems)
+            {
+                Assert.Contains(colorItem.Value, colorItem.Text);
+                Assert.Equal(Color.FromName(colorItem.Value).ToArgb(), colorItem.BackgroundColor.ToArgb());
+            }
+        }
+
+        [Fact]
+        public void The_count_of_style_links_should_equal_to_the_count_of_color_items()
+        {
+            Assert.NotEqual(0, this.page.StyleLinks.Count());
+            Assert.Equal(this.page.ColorItems.Count(), this.page.StyleLinks.Count());
+        }
+
+        [Fact]
+        public void Add_new_color_should_request_new_style_link()
+        {
+            string red = "red";
+            Assert.Equal(red, this.page.InputBox.GetAttribute("value"));
+
+            var previousStyleLinks = this.page.StyleLinks;
+
+            this.page.AddButton.Click();
+            var currentStyleLinks = this.page.StyleLinks;
+
+            Assert.Equal(this.page.ColorItems.Count(), this.page.StyleLinks.Count());
+
+            var addedStyleLinks = currentStyleLinks.Except(previousStyleLinks, StylePage.StyleLink.Comparer);
+            Assert.Equal(1, addedStyleLinks.Count());
+
+            var addedStyleLink = addedStyleLinks.Single();
+            Assert.EndsWith(red, addedStyleLink.Href);
+        }
+
+        [Fact]
+        public void Remove_existing_color_should_remove_corresponding_style_link()
+        {
+            string yellow = "yellow";
+            var firstColorItem = this.page.ColorItems.First();
+            Assert.Equal(yellow, firstColorItem.Value);
+
+            var previousStyleLinks = this.page.StyleLinks;
+
+            firstColorItem.Remove();
+            var currentStyleLinks = this.page.StyleLinks;
+
+            Assert.Equal(this.page.ColorItems.Count(), this.page.StyleLinks.Count());
+
+            var removedStyleLinks = previousStyleLinks.Except(currentStyleLinks, StylePage.StyleLink.Comparer);
+            Assert.Equal(1, removedStyleLinks.Count());
+            Assert.False(currentStyleLinks.Any(link => link.Href.EndsWith(yellow)));
+        }
+    }
+}
