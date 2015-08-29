944bb8629c3ae113d015c5b88a80b6c8064ddd1b
Junle Li <lijunle@gmail.com>
2015-08-29 14:19:18 +0800

    Add title page test cases.

---
+++ Nancy.ViewEngines.React.Tests/Pages/TitlePage.cs
@@ -0,0 +1,15 @@
+﻿namespace Nancy.ViewEngines.React.Tests.Pages
+{
+    using OpenQA.Selenium;
+
+    internal class TitlePage : PageBase
+    {
+        internal IWebElement TitleBox =>
+            this.Driver.FindElement(By.CssSelector("input[type=text]"));
+
+        internal IWebElement UpdateButton =>
+            this.Driver.FindElement(By.CssSelector("input[type=button]"));
+
+        protected override string Path => "title";
+    }
+}
