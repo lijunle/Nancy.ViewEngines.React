73222689347110937a21e231e3f245475dcd26d7
Junle Li <lijunle@gmail.com>
2015-09-02 02:27:43 +0800

    Add test case to cover example work fine.

---
+++ Nancy.ViewEngines.React.Tests/Pages/SuperSimplePage.cs
@@ -0,0 +1,12 @@
+﻿namespace Nancy.ViewEngines.React.Tests.Pages
+{
+    using OpenQA.Selenium;
+
+    internal class SuperSimplePage : PageBase
+    {
+        internal string Content =>
+            this.Driver.FindElement(By.TagName("body")).Text;
+
+        protected override string Path => "super-simple";
+    }
+}
