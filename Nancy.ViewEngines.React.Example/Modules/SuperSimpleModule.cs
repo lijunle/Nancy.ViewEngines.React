5e9de5c5fcd44b709ce181fec7897358518019ef
Junle Li <lijunle@gmail.com>
2015-09-02 02:21:56 +0800

    Add example to demonstrate that React view engine co-exist with other view engines.

---
+++ Nancy.ViewEngines.React.Example/Modules/SuperSimpleModule.cs
@@ -0,0 +1,15 @@
+﻿namespace Nancy.ViewEngines.React.Example.Modules
+{
+    public class SuperSimpleModule : NancyModule
+    {
+        public SuperSimpleModule()
+        {
+            this.Get["super-simple"] = _ => new SuperSimpleModel();
+        }
+
+        private class SuperSimpleModel
+        {
+            public string Name => "Super Simple view engine";
+        }
+    }
+}
