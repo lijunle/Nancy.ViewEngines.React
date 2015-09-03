5dabae9c15a7a979a3de0bb025e1d7f6bf68f956
Junle Li <lijunle@gmail.com>
2015-09-03 15:14:27 +0800

    Add exception handling test case.

---
+++ Nancy.ViewEngines.React.Tests/ExceptionTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Nancy.ViewEngines.React.Tests
+{
+    using Pages;
+    using Xunit;
+
+    public class ExceptionTests : TestBase
+    {
+        public ExceptionTests(Fixture fixture)
+            : base(fixture)
+        {
+        }
+
+        [Fact]
+        public void Throwing_exception_should_render_error_page()
+        {
+            var page = this.GoTo<ExceptionPage>();
+
+            Assert.Equal("500 - InternalServerError", page.ErrorCaption);
+            Assert.StartsWith("JavaScriptEngineSwitcher.Core.JsRuntimeException", page.ErrorDetails);
+            Assert.Contains("An exception is thrown in JavaScript code", page.ErrorDetails);
+        }
+    }
+}
---
+++ Nancy.ViewEngines.React.Tests/Pages/ExceptionPage.cs
@@ -0,0 +1,15 @@
+﻿namespace Nancy.ViewEngines.React.Tests.Pages
+{
+    using OpenQA.Selenium;
+
+    internal class ExceptionPage : PageBase
+    {
+        internal string ErrorCaption =>
+            this.Driver.FindElement(By.CssSelector("#errorText > h1")).Text;
+
+        internal string ErrorDetails =>
+            this.Driver.FindElement(By.Id("errorContents")).Text;
+
+        protected override string Path => "exception";
+    }
+}
