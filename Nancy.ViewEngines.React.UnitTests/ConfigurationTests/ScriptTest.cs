15312228c99f21caa5d88be60bb7767edf4e22ec
Junle Li <lijunle@gmail.com>
2015-09-08 02:59:58 +0800

    Add script configuration tests.

---
+++ Nancy.ViewEngines.React.UnitTests/ConfigurationTests/ScriptTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Nancy.ViewEngines.React.UnitTests.ConfigurationTests
+{
+    using System.Linq;
+    using Xunit;
+
+    public class ScriptTest : TestBase
+    {
+        public ScriptTest()
+            : base("Script.config")
+        {
+        }
+
+        [Fact]
+        public void Script_configuration_should_load_properties()
+        {
+            Assert.Equal("script-dir", this.Configuration.Script.Dir);
+            Assert.Equal("script-name.js", this.Configuration.Script.Name);
+            Assert.Equal(2, this.Configuration.Script.Extensions.Count);
+        }
+
+        [Fact]
+        public void Script_configuration_should_load_extensions()
+        {
+            var extensions = this.Configuration.Script.Extensions
+                .Cast<ReactConfiguration.ExtensionElement>()
+                .Select(x => x.Name);
+
+            Assert.Contains("es", extensions);
+            Assert.Contains("jsx", extensions);
+        }
+    }
+}
